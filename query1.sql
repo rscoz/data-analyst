7104d462ddd931414f41b08ee7ab2a255f1eb87f
Rafael Scoz <rafaelscoz@gmail.com>
2019-12-03 01:21:33 -0300

    remove query1 file

--- query1.sql
+++
@@ -1,5 +0,0 @@
-SELECT a.card_number, a.card_family, SUM(b.value), COUNT(b.id)
-FROM
-    cards as a
-LEFT JOIN transactions as b ON a.card_number=b.card_number
-GROUP BY a.card_number, a.card_family;
