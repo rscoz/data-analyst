e713bac3a2257b7955f0af82b5cdb1557da6dac7
Rafael Scoz <rafaelscoz@gmail.com>
2019-12-03 01:12:44 -0300

    count transactions id

--- query1.sql
+++ query1.sql
@@ -1,4 +1,4 @@
-SELECT a.card_number, a.card_family, SUM(b.value)
+SELECT a.card_number, a.card_family, SUM(b.value), COUNT(b.id)
 FROM
     cards as a
 LEFT JOIN transactions as b ON a.card_number=b.card_number
