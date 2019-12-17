1d9c78121ab25ee0ad821339f2d13d4800b3124b
Rafael Scoz <rafaelscoz@gmail.com>
2019-12-17 00:07:37 -0300

    use sqlalchemy and refactor

--- v2/app.py
+++ v2/app.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python
 
 import pandas as pd
-import psycopg2
+import sqlalchemy
+from sqlalchemy import create_engine
 
-from config import config
+engine = create_engine(
+    "postgresql://read_only_user:banking123@db-stone-challenge.cjepwwjnksng.us-east-1.rds.amazonaws.com:5432/postgres"
+)
 
 
 def main():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
+    csv1()
 
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
 
-        # create a cursor
-        cur = conn.cursor()
+def csv1():
 
-        # Dataset 1
-        sql = """SELECT a.card_number, a.card_family, SUM(b.value) as sum, COUNT(b.id) as transactions
-            FROM
-                cards as a
-            LEFT JOIN transactions as b ON a.card_number=b.card_number
-            GROUP BY a.card_number, a.card_family;"""
+    sql = """SELECT a.card_number, a.card_family, SUM(b.value) as sum, COUNT(b.id) as transactions
+        FROM
+            cards as a
+        LEFT JOIN transactions as b ON a.card_number=b.card_number
+        GROUP BY a.card_number, a.card_family;"""
 
-        result = pd.read_sql_query(sql, conn)
+    result = pd.read_sql_query(sql, engine)
 
-        # close the communication with the PostgreSQL
+    df = pd.DataFrame(result)
 
-        df = pd.DataFrame(result)
-
-        df.to_csv("dataframe1.csv")
-
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    df.to_csv("dataframe1.csv")
 
 
 if __name__ == "__main__":
